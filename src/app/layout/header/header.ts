898ef14fd6a0facbe26e18e00b0890fc24fc5249
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-08-07 09:18:25 -0300

    Enhance header component by adding missing imports for RouterLink, MatBadgeModule, and MatButtonModule

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
 import { Nav } from '../nav/nav';
+import { Cart } from '../../shared/cart';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, Nav],
+  imports: [MatToolbarModule, MatIconModule, MatBadgeModule, MatButtonModule, RouterLink, Nav],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   protected readonly title = 'Padaria';
+
+  constructor(protected readonly cart: Cart) {}
 }
