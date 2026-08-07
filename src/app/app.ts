1eacb5caa040a8bd4358a2e45311ff1b9dd00e8d
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-08-07 09:17:01 -0300

    Remove unused section imports and retain only necessary components for app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
-import { Doces } from './sections/doces/doces';
-import { Salgados } from './sections/salgados/salgados';
-import { Folheados } from './sections/folheados/folheados';
-import { Cafes } from './sections/cafes/cafes';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Doces, Salgados, Folheados, Cafes],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
